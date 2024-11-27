55c77cd2384024af4da8d539cc1835025edfc89e
Robert Kump <kumprj@miamioh.edu>
2024-11-27 16:00:25 -0600

    code cleanup

--- src/get_name_and_number.py
+++ src/get_name_and_number.py
@@ -1,14 +1,17 @@
 import boto3
 import json
+import os
+# account_sid = os.environ["AWS_ACCESS_KEY_ID"]
+# auth_token = os.environ["AWS_ACCESS_KEY_SECRET"]
 
 def getNameAndNumber():
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('BlackhawksCfa')
     response = table.scan()
-
     nameList = []
     for i in response['Items']:
-        nameList.append(i['Name'])
+        nameList.append((i['Name'], i['Phone']))
+    print(nameList)
     return nameList
 
 def main():
