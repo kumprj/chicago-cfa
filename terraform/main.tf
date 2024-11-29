e47c2f3e0db37281223d079cecbee8c1e861bcb3
Robert Kump <kumprj@miamioh.edu>
2024-11-28 21:46:33 -0500

    add terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+
+
+module "lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = "blackhawks-cfa-texter"
+  description   = "Lambda Function to send text alerts about free Chick-fil-a breakfast when the blackhawks score."
+  handler       = "free_cfa.lambda_handler"
+  runtime       = "python3.13"
+  
+  create_package         = false
+  local_existing_package = "../package.zip" # Dummy zip to be updated via a Github Action
+
+#   tags = {
+#     Name = "bezos scheduler"
+#   }
+}
