4f937647209b3cb4ca98150990172d7ac823a0ae
Robert Kump <rkump24@gmail.com>
2024-12-08 23:02:07 -0600

    try to rebuild again

--- backend/index.mjs
+++ backend/index.mjs
@@ -95,7 +95,7 @@ export const handler = async (event) => {
 
 async function createMessage(phone) {
 
-  const message = await client.messages.create({
+  const message = client.messages.create({
 
     body: "You're in for Blackhawks Chick-fil-a breakfast alerts! Reply STOP to unsubscribe. Msg&Data Rates May Apply",
 
@@ -103,7 +103,8 @@ async function createMessage(phone) {
 
     to: "+1" + phone,
 
-  });
+  }).then(message => console.log(message.status))
+    .done();
 
 
   // console.log("You're in for Blackhawks Chick-fil-a breakfast alerts! Reply HELP for help. Reply STOP to unsubscribe. Msg&Data Rates May Apply");
@@ -112,7 +113,7 @@ async function createMessage(phone) {
 
 async function deleteMessage(phone) {
 
-  const message = await client.messages.create({
+  const message = client.messages.create({
 
     body: "Your data has been successfully deleted from the database. Reply HELP for help. Reply STOP to unsubscribe. Msg&Data Rates May Apply",
 
@@ -120,7 +121,10 @@ async function deleteMessage(phone) {
 
     to: "+1" + phone,
 
-  });
+  }).then(message => console.log(message.status))
+
+    .done();
+
 
 
   // console.log("Your data has been successfully deleted from the database. Reply HELP for help. Reply STOP to unsubscribe. Msg&Data Rates May Apply");
