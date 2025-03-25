cfee3e6071a6d245bcb57945a265f939265e2ce9
Robert Kump <rkump24@gmail.com>
2025-03-24 20:12:17 -0500

    add comments for this file and change to type synschronized

---
+++ src/cubs.py
@@ -0,0 +1,14 @@
+# >>> mlb = mlbstatsapi.Mlb()
+# >>> game = mlb.get_game(662242)
+# >>>
+# >>> play_by_play = game.livedata.plays
+# >>> line_score = game.livedata.linescore
+# >>> box_score = game.livedata.boxscore
+# ^^ live data
+
+# Game box score:
+# >>> mlb = mlbstatsapi.Mlb()
+# >>> boxscore = mlb.get_box_score(662242)
+
+
+# https://github.com/zero-sum-seattle/python-mlb-statsapi
