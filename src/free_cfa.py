8e53f3e14259d3479118a787088f6e288c072462
Robert Kump <rkump24@gmail.com>
2025-03-26 08:36:50 -0500

    patch timezone

--- src/free_cfa.py
+++ src/free_cfa.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import requests
 import datetime
 from datetime import datetime, timedelta
@@ -7,7 +7,7 @@ from send_text import send_text
 
 # Loop through all the games from today to check if we have any that are Chicago/United Center.
 def get_game_id():
-    today = datetime.today() - timedelta(days=1)
+    today = datetime.now(timezone.utc) - timedelta(days=1)
     year = today.strftime("%Y")
     month = today.strftime("%m")
     day = today.strftime("%d")
